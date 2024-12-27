ea7c6f995e1d423f71f30585336e2d60196b195b
Prashant Kafle <prashantkafle7738@gmail.com>
2024-12-27 11:11:08 +0545

    first comment

---
+++ blog/posts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from . import models
+
+class CreatePost(forms.ModelForm):
+    
+    class Meta:
+        model = models.Post
+        fields = ('title','body','slug','image')
+        
+        
+    
